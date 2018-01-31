84b51cb1b5be6c4641e12dbe557cb82bcdeba845
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-01-31 12:52:03 +0000

    pushing up new code

--- src/main/scala/ReversedSeq.scala
+++ src/main/scala/ReversedSeq.scala
@@ -1,7 +1,7 @@
 object ReversedSeq extends App {
 
   def makeList (Number : Int) : List[Int] = {
-    val x = (0 to Number).toList.reverse
+    val x = (20 to Number).toList.reverse
     x
   }
 println(makeList(50))
